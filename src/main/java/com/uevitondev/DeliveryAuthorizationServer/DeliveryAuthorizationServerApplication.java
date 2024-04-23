a547345423abe12572391c642dcde3c47374f018
uevitondev <uevitonsantos@outlook.com>
2024-04-23 14:36:11 -0300

    project created

---
+++ src/main/java/com/uevitondev/DeliveryAuthorizationServer/DeliveryAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.uevitondev.DeliveryAuthorizationServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryAuthorizationServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uevitondev/DeliveryAuthorizationServer/DeliveryAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uevitondev.DeliveryAuthorizationServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
